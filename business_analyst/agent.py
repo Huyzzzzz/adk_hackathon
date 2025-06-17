bb37c5be3327ca561cc6bd86ad123d09607a0f1e
TrungNT167 <TrungNT167@fpt.com>
2025-06-17 15:26:52 +0700

    remove file

--- business_analyst/agent.py
+++ business_analyst/agent.py
@@ -5,8 +5,7 @@ from .sub_agents.ur_agent.agent import ur_agent
 from .sub_agents.ac_agent.agent import ac_agent
 from .sub_agents.do_agent.agent import do_agent
 from .sub_agents.uc_agent.agent import uc_agent
-from .tools.file import read_file_tool, find_file_tool, save_input_tool
-from .tools.save_agent_ouput import save_actors_ouput_tool, save_data_objects_ouput_tool, save_use_cases_ouput_tool, save_user_requirements_ouput_tool
+
 
 
 parallel_analysis_agent = ParallelAgent(
@@ -36,20 +35,18 @@ business_analyst_coordinator = LlmAgent(
     - Do NOT generate, transform, or explain outputs by yourself, you must use your tools and 'sequential_agent'.
     - Do NOT skip, reorder, or reinterpret steps.
     - Your job is to start the defined workflow, if user input is not clear , ask them if they want to upload file to analyse 
+    
     **FILE WORKFLOW**
 
     User will upload a file.
     If no file is uploaded , you must ask: "Please upload the file you want to analyze."
-    If file upload is available:
-    - First, use `save_input_tool` to store the uploaded file.
-    - Then, use `find_file_tool` to return the uploaded file path. You MUST use this tool
-    - Finally, use `read_file_tool` to extract file content from 'find_file_tool'.
-    - Display the preview to the user and ask user to confirm.
+    - Display the preview content of the uploaded file in 'business_analyst_output'to the user and ask user to confirm.
     If user confirms, you must proceed to ANALYSIS WORKFLOW.
     If user does not confirm, you must restart file workflow.
 
     **ANALYSIS WORKFLOW ** NOTE: If any of the following outputs already exist in `State`, you MUST return them immediately to the user and use them for subsequent steps. DO NOT re-run the corresponding agents.
-    Pass 'extracted_content' to `sequential_agent` to control execution in the following logical order:
+    
+    Pass uploaded file's content from 'business_analyst_output' to `sequential_agent` to control execution in the following logical order:
     First, invoke `ur_agent` to extract user requirements, Store the result in `user_requirements_extraction`
     Then, invoke `parallel_analysis_agent` to run:
     - `ac_agent` to extract actors from `user_requirements_extraction`, Store the result in `actors_output`
@@ -61,30 +58,12 @@ business_analyst_coordinator = LlmAgent(
     - `data_objects_output`
     - Store the result in `use_cases_output`.
     CAUTION: In the last step of the ANALYSIS WORKFLOW, you MUST call `uc_agent` to generate use cases based on the extracted user requirements, actors, and data objects.
-
-    **OUTPUT SAVING WORKFLOW**
-
-    First, make sure the following outputs to the user in Markdown:
-    - `user_requirements_extraction`
-    - `actors_output`
-    - `data_objects_output`
-    - `use_cases_output`
-
-    Then, save each output using the appropriate tools:
-    - `save_user_requirements_ouput_tool`
-    - `save_actors_ouput_tool`
-    - `save_data_objects_ouput_tool`
-    - `save_use_cases_ouput_tool`
-
-    Finally, confirm completion with the user:
-    - Message: "All outputs have been successfully saved."
-    - List all saved file paths.
     """
 
     ,
     sub_agents=[sequential_agent],
     output_key="business_analyst_output",
-    tools=[read_file_tool, find_file_tool, save_input_tool, save_actors_ouput_tool, save_data_objects_ouput_tool, save_use_cases_ouput_tool, save_user_requirements_ouput_tool],
+
 )
 
 root_agent = business_analyst_coordinator
